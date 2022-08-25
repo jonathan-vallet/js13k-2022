0cb13e2d1fcf4582f4bb7a4ee1b54af686e17b82
Loup Aubour <laubour@arneogroup.com>
2022-08-25 15:13:42 +0200

    Affichage d'un timer de compte à rebours de partie

--- sources/scripts/02-utils.js
+++ sources/scripts/02-utils.js
@@ -40,3 +40,26 @@ function gaussianRandom() {
   } // resample between 0 and 1
   return num;
 }
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var fiveMinutes = 60 * 5,
+      display = document.querySelector('#time');
+  startTimer(fiveMinutes, display);
+};
